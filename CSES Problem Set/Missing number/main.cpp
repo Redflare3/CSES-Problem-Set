d837c45d72a1050512344c94c4be1877278421ad
Solar <141292528+Redflare3@users.noreply.github.com>
2025-11-05 20:42:40 +0700

    Add files via upload

---
+++ CSES Problem Set/Missing number/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+
+    int n  =0;
+    long long x = 0;
+
+    cin >> n;
+
+
+    for (int i = 1 ; i <= n; i++){
+        x += i;
+    }
+
+    long long x_2 = 0;
+    for(int i = 0; i < n-1;i++){
+        int x_2_i;
+        cin >> x_2_i;
+        x_2 += x_2_i;
+    }
+
+    cout << x - x_2;
+}
