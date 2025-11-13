505978ea0a7d5d8edc9b55ee80da4cbd1f50ec0b
Solar <141292528+Redflare3@users.noreply.github.com>
2025-11-13 18:48:56 +0700

    Refactor Weird Algorithm implementation

--- CSES Problem Set/Weird Algorithm/main.cpp
+++ CSES Problem Set/Weird Algorithm/main.cpp
@@ -10,16 +10,15 @@ int main()
             cout << n << " ";
          if (n % 2 == 0 ){
         n = n / 2;
-    }else{
-            n  = (n * 3) + 1;
-        n = (n * 3) + 1;
-    }
+             
+        }else {n = (n * 3) + 1;}
     }
 
     cout << n;
 
     return 0;
 }
+
 
 
 
