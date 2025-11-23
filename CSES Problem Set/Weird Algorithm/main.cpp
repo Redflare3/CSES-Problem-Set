0b0d657f47669c06827ad43e6c687edaf5bd56b6
Solar <141292528+Redflare3@users.noreply.github.com>
2025-11-23 19:58:33 +0700

    Change int to long long for variable n

--- CSES Problem Set/Weird Algorithm/main.cpp
+++ CSES Problem Set/Weird Algorithm/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n = 0;
+    long long n = 0;
 
     cin >> n;
     while (n != 1)  {
@@ -17,6 +17,7 @@ int main()
     cout << n;
     return 0;
 }
+
 
 
 
